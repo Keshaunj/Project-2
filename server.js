d44107baef1d57c9bedfe161bae94fbe0e42e310
Khushnud Sadikov <vitasmile@gmail.com>
2024-10-01 05:00:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
+const CarController = require("./controllers/Car.js");
+
 const methodOverride = require("method-override");
 const morgan = require("morgan");
-const path = require('path');
-//setting the view engine
-app.set('View engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-//server static files in art dir
-app.use('/art', express.static(path.join(__dirname,'art')));
+const path = require("path");
 
+const session = require('express-session');
 
 
 // Set the port from environment variable or default to 3000
@@ -26,26 +24,61 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
 
+const Car = require("./models/Car");
+
+// configure view engine
+app.set("view engine", "ejs");
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
+app.use(methodOverride("_method"));
 app.use(morgan('dev'));
 
+app.use(express.static(path.join(__dirname, "public")));
+
+// new
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+
 //GET
-app.get("/", async (req, res) => {
-  res.render("index.ejs");
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    user: req.session.user,
+  });
 });
+
+app.get("/vip-lounge", (req, res) => {
+  if (req.session.user) {
+    res.send(`Welcome to the party ${req.session.user.username}.`);
+  } else {
+    res.send("Sorry, no guests allowed.");
+  }
+});
+
+app.get("/Car/new", CarController.getNewForm);
+app.get("/Car/:id", CarController.getOneCar);
+app.get("/Car", CarController.getAllCars);
+app.post("/Car", CarController.createCar);
+app.delete("/Car/:id", CarController.deleteCar);
+app.get("/Car/:CarId/edit", CarController.getEditForm);
+app.get("/Car/:CarId/edit", CarController.getEditForm);
+
+
 
 app.use("/auth", authController);
 
-// secong page/landing page
-app.get('/second',(req,res)=>{
-  res.render('second.ejs');
- });
- 
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
